98954a67ca033ce1d765d4e498da51c63bfbca0e
Radha Gobinda Mishra <sipumishra94@gmail.com>
2019-09-30 01:10:03 +0530

    []

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -32,6 +32,11 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            //});
+           // app.UseMvc();
         }
     }
 }
